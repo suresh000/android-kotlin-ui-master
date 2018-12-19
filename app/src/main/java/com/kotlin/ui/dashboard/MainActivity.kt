05bd459aa62d1c32a6cfda0b821099aeac01b357
Suresh Chandak <suresh.chandak@paynearby.in>
2018-12-19 13:19:19 +0530

    add -> app theme color & data binding

---
+++ app/src/main/java/com/kotlin/ui/dashboard/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kotlin.ui.dashboard
+
+import android.os.Bundle
+import com.kotlin.ui.BaseActivity
+import com.kotlin.ui.R
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
